82a6fe354f789312cf099bdceab34424b6390c38
ashutoshgupta2025-cmyk <ashutosh.gupta2025@glbajajgroup.org>
2026-06-20 17:42:05 +0530

    Create 02_problem.c

---
+++ Day_12/02_problem.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void armstrong(int n)
+{
+    int sum = 0, rem, temp = n;
+
+    while (n > 0)
+    {
+        rem = n % 10;
+        sum = sum + (rem * rem * rem);
+        n = n / 10;
+    }
+
+    if (sum == temp)
+        printf("Armstrong Number");
+    else
+        printf("Not an Armstrong Number");
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    armstrong(num);
+
+    return 0;
