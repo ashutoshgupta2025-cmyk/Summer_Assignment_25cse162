ac2f68e349386c4cc331ec834b2c8c4339419dfc
ashutoshgupta2025-cmyk <ashutosh.gupta2025@glbajajgroup.org>
2026-06-20 17:44:14 +0530

    Create 04_problem.c

---
+++ Day_12/04_problem.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void perfect(int n)
+{
+    int i, sum = 0;
+
+    for (i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+            sum = sum + i;
+    }
+
+    if (sum == n)
+        printf("Perfect Number");
+    else
+        printf("Not a Perfect Number");
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    perfect(num);
+
+    return 0;
+}
