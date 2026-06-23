53064bd37cd7f155120c34fe0ef8903692b9afb0
ashutoshgupta2025-cmyk <ashutosh.gupta2025@glbajajgroup.org>
2026-06-23 17:45:18 +0530

    Create 04_problem.c

---
+++ Day_15/04_problem.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() 
+{
+    int arr[] = {0, 1, 0, 3, 12};
+    int n = 5;
+
+    int index = 0;
+
+    for (int i = 0; i < n; i++)
+     {
+        if (arr[i] != 0) {
+            arr[index++] = arr[i];
+        }
+    }
+
+    while (index < n)
+     {
+        arr[index++] = 0;
+    }
+
+    printf("Array after moving zeroes: ");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
