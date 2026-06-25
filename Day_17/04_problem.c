46f905ff8023a92df93d5d0652e20b7a94b1ef37
ashutoshgupta2025-cmyk <ashutosh.gupta2025@glbajajgroup.org>
2026-06-25 11:57:12 +0530

    Create 04_problem.c

---
+++ Day_17/04_problem.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+ {
+    int arr[] = {1, 2, 2, 3, 4, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array after removing duplicates: ");
+
+    for (int i = 0; i < size; i++) 
+    {
+        int duplicate = 0;
+
+        for (int j = 0; j < i; j++) 
+        {
+            if (arr[i] == arr[j]) 
+            {
+                duplicate = 1;
+                break;
+            }
+        }
+
+        if (!duplicate) 
+        {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
