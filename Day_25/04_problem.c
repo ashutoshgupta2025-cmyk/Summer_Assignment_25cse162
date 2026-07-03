51e6dd83db568b516d4db843776b12b20b7f91a8
ashutoshgupta2025-cmyk <ashutosh.gupta2025@glbajajgroup.org>
2026-07-03 11:59:48 +0530

    Create 04_problem.c

---
+++ Day_25/04_problem.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+ {
+    char words[5][20], temp[20];
+
+    printf("Enter 5 words:\n");
+
+    for (int i = 0; i < 5; i++)
+        scanf("%s", words[i]);
+
+    for (int i = 0; i < 5; i++)
+     {
+        for (int j = i + 1; j < 5; j++)
+         {
+            if (strlen(words[i]) > strlen(words[j]))
+             {
+                strcpy(temp, words[i]);
+                strcpy(words[i], words[j]);
+                strcpy(words[j], temp);
+            }
+        }
+    }
+
+    printf("Words sorted by length:\n");
+
+    for (int i = 0; i < 5; i++)
+        printf("%s\n", words[i]);
+
+    return 0;
+}
